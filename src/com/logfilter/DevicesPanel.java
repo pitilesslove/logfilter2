97088367b82a152ac4b60a9aa517f9d568df64d8
computer99 <pitilesslove@gmail.com>
2024-11-19 18:16:14 +0900

    init commit

---
+++ src/com/logfilter/DevicesPanel.java
@@ -0,0 +1,9 @@
+package com.logfilter;
+
+import javax.swing.JPanel;
+
+public class DevicesPanel extends JPanel
+{
+    private static final long serialVersionUID = 1L;
+    
+}
---
+++ src/com/logfilter/ILogParser.java
@@ -0,0 +1,21 @@
+package com.logfilter;
+
+import java.awt.Color;
+
+/**
+ * 
+ */
+
+
+/**
+ * 
+ */
+public interface ILogParser
+{
+    public static final int TYPE_ANDROID_DDMS   = 0;
+    public static final int TYPE_ANDROID_LOGCAT = 1;
+    
+    public LogInfo parseLog(String strText);
+    public Color   getColor(LogInfo logInfo);
+    public int     getLogLV(LogInfo logInfo);
+}
